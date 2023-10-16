e6076d4a307eee1cd9176292dc9cf24f0bc80d2b
stla <laurent_step@yahoo.fr>
2023-10-16 14:04:58 +0200

    more unit tests Rogers-Ramanajuan

--- tests/testthat/test-RogersRamanajuan.R
+++ tests/testthat/test-RogersRamanajuan.R
@@ -11,3 +11,27 @@ test_that("A value of the alternating Rogers-Ramanajuan function", {
     RRa(exp(-pi)), as.complex(expected)
   )
 })
+
+test_that("Relation to Dedekind eta 1", {
+  tau <- 0.1 + 1i
+  q <- exp(1i * pi * tau)
+  expect_equal(
+    1/RR(q^2) - RR(q^2), 1 + eta(tau/5) / eta(5*tau)
+  )
+})
+
+test_that("Relation to Dedekind eta 1", {
+  tau <- 0.1 + 1i
+  q <- exp(1i * pi * tau)
+  expect_equal(
+    1/RR(q^2) - RR(q^2), 1 + eta(tau/5) / eta(5*tau)
+  )
+})
+
+test_that("Relation to Dedekind eta 2", {
+  tau <- 0.1 + 1i
+  q <- exp(1i * pi * tau)
+  expect_equal(
+    1/RR(q^2)^5 - RR(q^2)^5, 11 + (eta(tau) / eta(5*tau))^6
+  )
+})
